14cd9e260671507b22fdb6a31b90ad9b04f9235c
함성훈 <shham@midasit.com>
2024-03-20 18:48:43 +0900

    Finish

--- practice01/google.js
+++ practice01/google.js
@@ -1,4 +1,19 @@
 function onSearch() {
+    const searchBox = document.querySelector('#search_box');
+    let url = 'https://google.com/search?q=' + searchBox.value;
+    let newTab = window.open(url, '_blank');
+}
+
+function onGmail() {
+    alert('Gmail');
+}
+
+function onImage() {
+    let newTab = window.open('https://www.google.com/imghp?hl=ko&tab=ri&ogbl', '_blank');
+}
+
+function onFeelingLucky() {
+    alert('I am Feeling Lucky!');
 }
 
 window.onload = function () {
