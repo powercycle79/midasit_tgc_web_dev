6c7dcb919d068d45b771455f2408bbf9e8063995
김용수 <kys0522@midasit.com>
2024-04-23 10:53:01 +0900

    practice02 Example - 배경색 지정 및 불필요 요소 제거

--- practice02/src/App.js
+++ practice02/src/App.js
@@ -16,28 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p onClick={()=>setName("TGC")}>
-          {name}
-        </p>
-
-        {
-          // 새로 정의한 <Todos/> 컴포넌트를 사용합니다. 이 컴포넌트는 바디가 필요없어서 self-closing tag로 사용합니다.
-        }
-        <Todos/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos/>
     </div>
   );
 }
