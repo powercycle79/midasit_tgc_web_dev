8fb720f4d8f24da675ea09c6561893ed6ca2938a
김용수 <kys0522@midasit.com>
2024-05-07 13:23:58 +0900

    practice04 Example - 동작하도록 수정.

--- practice04/todos-web/src/components/Todos.js
+++ practice04/todos-web/src/components/Todos.js
@@ -1,33 +1,48 @@
 import { useState } from "react";
 import './Todos.css';
-import useTodos from '../hooks/useTodos'
 import Todo from "./Todo";
+import {addTodo, delTodo, getTodos, updateTodo} from "../api/api";
 
 // 새로운 컴포넌트를 정의합니다.
 function Todos() {
 
-    const [counter, setCounter] = useState(1);
-    // api 호출을 통해 받아온 데이터를 todos라는 이름으로 사용합니다. (내부에서 useState, useEffect 사용됨)
-    const todos = useTodos(counter);
+    const [newContent, setNewContent] = useState('');
+    const [todos, setTodos] = useState(getTodos());
 
     const onClickAdd = () => {
-        setCounter(counter + 1);
+        const newTodo = addTodo(newContent, false);
+        setTodos([...todos, newTodo]);
+
     }
     const onClickDelete = () => {
-        if(counter > 1) setCounter(counter - 1);
+        if(todos.length === 0) return;
+
+        delTodo(todos[todos.length - 1].id);
+        setTodos(todos.slice(0, todos.length - 1));
+    }
+
+    const setTodo = (todo)=> {
+        updateTodo(todo);
+        setTodos(todos.map(t=>t.id === todo.id ? todo : t));
     }
 
     return (
         <div className="TodoListApp">
             <h1>TODOs</h1>
             <div className="TodoControl">
+                <input
+                    type="text"
+                    onChange={(e) => setNewContent(e.target.value)}
+                    value={newContent}
+                    className="NewTodoEdit"
+                ></input>
                 <button className = "TodoButton" onClick={onClickAdd}>추가</button>
                 <button className = "TodoButton" onClick={onClickDelete}>제거</button>
             </div>
             <div className="Todos">
                 <ul className="TodoList">
                     { // todos 배열을 순회하며 각각의 요소를 <li> 태그를 사용하여 출력합니다.
-                        todos.map((todo)=><Todo todo={todo} />)
+                        todos.map((todo)=><Todo key={todo.id} todo={todo} setTodo={setTodo} />)
                     }
                 </ul>
             </div>
